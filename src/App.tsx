cc0244f161aecff8fd530409cc7bea3b20f60312
Roshni-Prajapati <rupit2330@gmail.com>
2023-11-18 12:50:24 +0530

    setup created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Box, Typography } from '@mui/material';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Box>
+        <Typography variant='h4'>
+          Lets get started
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+export default App;
